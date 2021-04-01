950be6c5f02f78bd6fa8233c58d8306513dcc09c
benchmark <bench.mark311@gmail.com>
2021-04-01 23:27:56 +0900

    update

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 'use strict';
 
 {
-    document.addEventListener("DOMContentLoaded", function(){
+    document.addEventListener('DOMContentLoaded', function(){
         document.getElementById('submit').addEventListener('click', (e) => {
             e.preventDefault();
-            let tExt_fb = document.getElementById('innertxt');
-            let fIzz_value = document.getElementById("Fizznum").value;
-            let bUzz_value = document.getElementById("Buzznum").value;
-            let fNum = 0;
-            let bNum = 0;
+            const text = document.getElementById('innertxt');
+            const fizzValue = document.getElementById('Fizznum').value;
+            const buzzValue = document.getElementById('Buzznum').value;
+            const result =  new RegExp(/^[0-9]+$/);
+            let fnum = 0;
+            let bnum = 0;
             let array = [];
-            let result_array = [];
-            let array_value = 0;
-            let result =  new RegExp(/^[0-9]+$/);
+            let resultArray = [];
+            let arrayValue = 0;
 
-            while(tExt_fb.firstChild){
-                tExt_fb.removeChild(tExt_fb.firstChild)
+            while (text.firstChild){
+                text.removeChild(text.firstChild);
             }
 
-            if((result.test(fIzz_value) === false) || (result.test(bUzz_value) === false)){
-                tExt_fb.insertAdjacentHTML('beforeend', "整数値を入力して下さい")
+            if ((result.test(fizzValue) === false) || (result.test(buzzValue) === false)){
+                text.insertAdjacentHTML('beforeend', '整数値を入力して下さい');
                 return;
             }
 
             for (let i = 0; i <= 100; i++) {
-                fNum += Number(fIzz_value);
-                array.push(fNum);
-                bNum += Number(bUzz_value);
-                array.push(bNum);
+                fnum += Number(fizzValue);
+                array.push(fnum);
+                bnum += Number(buzzValue);
+                array.push(bnum);
             }
             function compareFunc(a, b){
                 return a - b;
             }
-            result_array = [...new Set(array)];
-            result_array.sort(compareFunc);
+            resultArray = [...new Set(array)];
+            resultArray.sort(compareFunc);
             
-            for(let i = 0; i < result_array.length; i++){
-                array_value = result_array[i];
-                if(array_value < 100){
-                    if((array_value % fIzz_value === 0) && (array_value % bUzz_value === 0)){
-                        tExt_fb.insertAdjacentHTML('beforeend', "FizzBuzz:" + array_value + "<br>"); 
+            for (let i = 1; i < 99; i++){
+                arrayValue = resultArray[i];
+                if (arrayValue < 100){
+                    if ((arrayValue % Number(fizzValue) === 0) && (arrayValue % Number(buzzValue) === 0)){
+                        text.insertAdjacentHTML('beforeend', 'FizzBuzz:' + arrayValue + '<br>'); 
                     }
-                    else if(array_value % fIzz_value === 0){
-                        tExt_fb.insertAdjacentHTML('beforeend', "Fizz:" + array_value+ "<br>"); 
+                    else if (arrayValue % Number(fizzValue) === 0){
+                        text.insertAdjacentHTML('beforeend', 'Fizz:' + arrayValue+ '<br>'); 
                     }
-                    else if(array_value % bUzz_value === 0){
-                        tExt_fb.insertAdjacentHTML('beforeend', "Buzz:" + array_value+ "<br>"); 
+                    else if (arrayValue % Number(buzzValue) === 0){
+                        text.insertAdjacentHTML('beforeend', 'Buzz:' + arrayValue+ '<br>'); 
                     }
                 }
             }         
