bbc6c0e5699beee85001e26ad40802e1d9c09af3
benchmark <bench.mark311@gmail.com>
2021-04-06 12:04:32 +0900

    fix

--- JavaScript課題③/script.js
+++ JavaScript課題③/script.js
@@ -8,17 +8,49 @@
             const text = document.getElementById('innertxt');
             const task = document.getElementById('task').value;
             
-            function todoSet(){ 
-                let comment = task;
-                let status = 0;
-                todoArray.push([comment, status]);
-                text.insertAdjacentHTML('beforeend', 
-                                        '<tr><td>' + ( (todoArray.length) - 1) + '</td><td>'
-                                        + task + 
-                                        '</td><td><input type="submit" value="作業中" id=""> </td><td><input type="submit" value="削除" id=""> </td></tr>'); 
+            function addTodo(){ 
+                const item = {
+                    comment: task,
+                    status: '作業中'
+                };
+                todoArray.push(item);
+                document.getElementById('task').value = "";
+                return item;
             }
             
-            todoSet();
+            let content = addTodo();
+
+            function displayTodo(){
+                const idTr = document.createElement("tr");
+                const idTd = document.createElement("td");
+                const commentTd = document.createElement("td");
+                idTr.appendChild(idTd);
+                idTr.appendChild(commentTd);
+                idTd.textContent = (todoArray.length) - 1;
+                commentTd.textContent = content.comment;
+                text.insertBefore(idTr, null);
+                let btn = createBtn();
+                idTr.appendChild(idTd, null);
+                idTr.appendChild(commentTd, null);
+                idTr.appendChild(btn.workingBtnTd, null);
+                idTr.appendChild(btn.deleteBtnTd, null);
+            }
+
+            function createBtn(){
+                const workingBtnTd = document.createElement("td");
+                const workingBtn = document.createElement('input');
+                const deleteBtnTd = document.createElement("td");
+                const deleteBtn = document.createElement('input');
+                workingBtn.type = 'submit';
+                workingBtn.value = '作業中'
+                deleteBtn.type = 'submit'
+                deleteBtn.value = '削除'
+                workingBtnTd.appendChild(workingBtn);
+                deleteBtnTd.appendChild(deleteBtn);
+                return {workingBtnTd, deleteBtnTd}
+            }
+
+            displayTodo();
 
         });
     });
